3979cf4edd11623e8a3b425202b1355f0a8887ff
andreamenon <menon.andrea@gmail.com>
2025-08-04 23:38:14 +0200

    modificato:             SCL/SCL.depend 	modificato:             SCL/bin/Debug/SCL 	modificato:             SCL/main.c 	modificato:             SCL/obj/Debug/main.o

--- SCL/main.c
+++ SCL/main.c
@@ -1,30 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#ifndef SCL_H
-#define SCL_H
+#ifndef SLL_H
+#define SLL_H
+// Singly Linked List
 
-// First: define the struct with structural name SCL
-struct SCL {
+// Define the structure of the information within a node of the singly linked list
+struct SLLInfo {
+    // We consider the simplest case: just one int declaration
     int data;
-    struct SCL *next;
 };
+// Define the relative data type
+typedef struct SLLInfo SLLInfoType;
 
-// Them: define the type SCL so that to avaid every time to write "struct SCL" but just SCL
-typedef struct SCL SCL;
+// Define the structure of the nodes of the singly linked list
+struct SLLNode {
+    SLLInfoType info;
+    struct SLLNode *next;
+};
+// Define the relative data type
+typedef struct SLLNode SLLNodeType;
 
-// Create SCL
-SCL* createSCL() {
-    SCL *scl = malloc(sizeof(*scl));
-    if (scl == NULL) {
-        printf("createSCL malloc error\n");
+// Define the singly linked list type as a pointer to the structure that defines the nodes
+typedef SLLNodeType * SLLType;
+
+// Create a SLL of 1 node
+SLLType SLLCreate() {
+    SLLType sll = malloc(sizeof(SLLNodeType));
+    if (sll == NULL) {
+        printf("SLLCreate malloc error\n");
         exit(EXIT_FAILURE);
     }
-    return scl;
+    return sll;
 }
 
 // Add en element to the SCL in First position
-void addInFirstPostoSCL(SCL **pSCL) {
+/*void addInFirstPostoSCL(SCL **pSCL) {
     SCL *scl = malloc(sizeof(*scl));
     if (scl == NULL) {
         printf("aaddInFirstPostoSCL malloc error\n");
@@ -32,21 +43,24 @@ void addInFirstPostoSCL(SCL **pSCL) {
     }
     scl->next = *pSCL;
     *pSCL = scl;
-}
+}*/
 
-#endif // SCL_H
+#endif // SLL_H
 
 int main()
 {
-    // Declare and allocation of the first SCL
-    SCL *scl = createSCL();
-    scl->data = 10;
-    scl->next = NULL;
-    printf("First SCL data: %d\n", scl->data);
-    addInFirstPostoSCL(&scl);
-    scl->data = 20;
-    printf("Second SCL data: %d %d\n", scl->data, scl->next->data);
-    free(scl);
+
+    // Declaration and allocation of a SLL
+    SLLType sll = SLLCreate();
+
+    // Initialize the content of the unique node
+    sll->info.data = 10;
+    sll->next = NULL;
+    printf("First SLL data: %d\n", sll->info.data);
+    //addInFirstPostoSCL(&scl);
+    //scl->data = 20;
+    //printf("Second SCL data: %d %d\n", scl->data, scl->next->data);
+    free(sll);
     /*SCL scl1;
     scl1.data = 12;
     scl1.next = NULL;
